00a4423cf7b51e955ab7d14da91c3354ee713d1e
Seth-Tipps <Seth.Tipps@coxautoinc.com>
2020-06-04 04:00:49 -0700

    fixed weird merge flags?

--- TestApi/WeatherForecast.cs
+++ TestApi/WeatherForecast.cs
@@ -20,9 +20,6 @@ namespace TestApi
             TemperatureC = rng.Next(-20, 55);
             Summary = GetSummary(TemperatureC);
         }
-
-<<<<<<< HEAD
-<<<<<<< HEAD
         public WeatherForecast(int days, int temp){
             Date = DateTime.Now;
             TemperatureC = SetTemperature(temp);
@@ -33,14 +30,6 @@ namespace TestApi
             if(temp == null){
                 return "No temperature recorded";
             }else if(temp >= 40){
-=======
-        public string GetSummary(int temp){
-            if(temp >= 40){
->>>>>>> Initial creation of WeatherForecast Project
-=======
-        public string GetSummary(int temp){
-            if(temp >= 40){
->>>>>>> c8122a3de0db456a9348f015441d2caa55f6ae7a
                 return "Sweltering";
             }else if(temp < 40 && temp >= 30){
                 return "Hot";
@@ -56,15 +45,9 @@ namespace TestApi
                 return "Freezing";
             }else return "Record temperature!";
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
 
         private int SetTemperature(int temp){
             return temp;
         }
-=======
->>>>>>> Initial creation of WeatherForecast Project
-=======
->>>>>>> c8122a3de0db456a9348f015441d2caa55f6ae7a
     }
 }
